0aad32650d71a972ba567aac32d63c69ec47d415
Jay Pandya <113614843+Infern07@users.noreply.github.com>
2024-09-01 18:12:48 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import { DoctorForm } from './components/DoctorForm'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [] = useState(0)
 
   return (
     <>
